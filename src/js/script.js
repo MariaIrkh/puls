9791e4a41dc7c0bfb0b90a7719f27b1d5cc5ca4e
Maria <irinarkhova@yandex.ru>
2024-03-25 00:58:32 +0400

    tabs

--- src/js/script.js
+++ src/js/script.js
@@ -8,13 +8,18 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icon/arrows/arrow-right.png" alt="arrow right"></button>',
         responsive: [
             {
-                breakpoint: 920,
-                settings: {
-                    // dots: true,
-                    arrows: false,
+              breakpoint: 920,
+              settings: {
+              // dots: true,
+              arrows: false,
               }
             },
    
         ]
     });
-  });
+    $('ul.catalog__tabs').on('click', 'li:not(.active)', function() {
+        $(this)
+          .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+          .closest('div.container').find('div.catalog__content').removeClass('catalog__content_activ').eq($(this).index()).addClass('catalog__content_activ');
+      });
+});
